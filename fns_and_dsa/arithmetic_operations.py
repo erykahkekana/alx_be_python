92ffc9f997388f44a97b5afa7497d0a2878b664d
Mogau Ramara <kekanaerykah@gmail.com>
2025-01-15 07:02:20 -0800

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3def perform_operation(num1, num2, operation):
+    """Perform basic arithmetic operations."""
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation"
+
+
