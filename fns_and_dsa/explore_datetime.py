a71d1a0e1f1d920ee5e945ffc1e349b9c4ff471d
Mogau Ramara <kekanaerykah@gmail.com>
2025-01-15 04:28:06 -0800

    dates and times

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%M-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_date}")
+
+def calculate_future_date():
+    try:
+        days_to_add = int(input("Enter the number of days to add to the current date: "))
+        current_date = datetime.now()
+        future_date = current_date + timedelta(days=days_to_add)
+        formatted_future_date = future_date.strftime("%Y-%M-%d")
+        print(f"Future date: {formatted_future_date}")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+def main():
+    display_current_datetime()
+    calculate_future_date()
+
+if __name__ == "__main__":
+    main()
