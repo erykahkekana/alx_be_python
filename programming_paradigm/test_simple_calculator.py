be8ba4b375cd9fce9e68944ed44e3cfb38a44935
Mogau Ramara <kekanaerykah@gmail.com>
2025-01-17 04:30:40 -0800

    test simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -28,14 +28,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(0, 5), 0)
         self.assertEqual(self.calc.multiply(-3, -2), 6)
 
-
-    
-
-    def test_divide(self):
+    def test_division(self):
         """Test the divide method."""
-        self.assertEqual(self.calc.divide(6, 3), 2)
-        self.assertEqual(self.calc.divide(5, 0), None)
-        self.assertAlmostEqual(self.calc.divide(7, 3), 7/3, places=7)
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        self.assertEqual(self.calc.divide(-10, -2), 5)
+        self.assertEqual(self.calc.divide(10, 0), None)
 
 if __name__ == "__main__":
     unittest.main()
